dc9e9fd7011091909a56ce55b353095742bd37b7
Andrew Summers <andrew.z.summers@gmail.com>
2017-10-11 11:38:27 -0500

    Updated mBuild tutorial - master

--- mbuild-examples/visualize.py
+++ mbuild-examples/visualize.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 from mbuild import Box
 import numpy as np
 
-def visualize(compound, show_bonds=False, box=None, verbose=False):
+def visualize(compound, show_bonds=True, show_ports=True, box=None,
+              verbose=False):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     if not box:
         boundingbox = compound.boundingbox
+        if not any(boundingbox.lengths):
+            boundingbox.lengths = np.ones(3) / 10
         longest_side = boundingbox.lengths.max()
         center = (boundingbox.maxs + boundingbox.mins) / 2
         mins = np.array([dim - longest_side/2 for dim in center])
@@ -25,15 +28,21 @@ def visualize(compound, show_bonds=False, box=None, verbose=False):
     size = {'C': 1250/volume, 'H': 400/volume, 'O': 1300/volume,
             'Si': 2000/volume}
 
-    n_particles = compound.n_particles
-    for i, pos in enumerate(compound.xyz):
+    n_particles = compound._n_particles(include_ports=show_ports)
+    for i, particle in enumerate(compound.particles(include_ports=show_ports)):
         if verbose:
             print('Rendering particle {} of {}'.format(i+1, n_particles))
-        name = compound[i].name
-        name = ''.join([j for j in name if not j.isdigit()])
-        ax.scatter(pos[0], pos[1], pos[2], c=colors[name],
-                   s=size[name], edgecolors='black')
+        pos = particle.pos
+        if particle.port_particle:
+            ax.scatter(pos[0], pos[1], pos[2], c='white', s=50/volume,
+                       edgecolors='black')
+        else:
+            name = particle.name
+            name = ''.join([j for j in name if not j.isdigit()])
+            ax.scatter(pos[0], pos[1], pos[2], c=colors[name],
+                       s=size[name], edgecolors='black', alpha=0.8)
     if show_bonds:
         for bond in compound.bonds():
-            ax.plot(bond[0].pos, bond[1].pos, color='black', alpha=0.5)
+            ax.plot(*np.stack((bond[0].pos, bond[1].pos), axis=1), color='black',
+                    alpha=0.75, marker='o', markersize=1/volume, linewidth=1.0)
     plt.show()
